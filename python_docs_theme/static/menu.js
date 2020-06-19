203b40abbbf0a38cfad7d9885983ad1df517edce
Olga <obulat@gmail.com>
2020-06-20 00:13:24 +0300

    Address code review issues
    - Remove unnecessary `show` call for searchbox.
    - Remove `box-sizing:border-box` for all elements.
    - Add `aria-label` to search input instead of visually-hiding labels for accessibility
    - Some style fixes

--- python_docs_theme/static/menu.js
+++ python_docs_theme/static/menu.js
@@ -3,19 +3,33 @@ document.addEventListener('DOMContentLoaded', function () {
     const toggler = document.querySelector('.toggler');
     const sideMenu = document.querySelector('.menu-wrapper');
     const doc = document.querySelector('.document');
+
     function closeMenu() {
         sideMenu.classList.remove('open');
-        toggler.checked = false;
+        toggler.classList.remove('active');
+        toggler.setAttribute("aria-expanded", 'false');
     }
-    toggler.addEventListener('change', function (e) {
-        if (toggler.checked) {
+    function isMenuOpen(){
+        return (' ' + sideMenu.className + ' ').indexOf(' open ') > -1;
+    }
+
+    sideMenu.addEventListener('click', function (event) {
+        // Close menu only if a link on the sideMenu is clicked
+        let target = event.target;
+        if (target.tagName !== 'a') return;
+        closeMenu();
+    })
+    toggler.addEventListener('click', function (e) {
+        if (!isMenuOpen()) {
             sideMenu.classList.add('open');
+            toggler.classList.add('active');
+            toggler.setAttribute("aria-expanded", 'true');
         } else {
             closeMenu();
         }
     });
     doc.addEventListener('click', function () {
-        if (toggler.checked) {
+        if (isMenuOpen()) {
             closeMenu();
         }
     })
