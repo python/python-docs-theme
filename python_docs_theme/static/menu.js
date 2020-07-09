10f43495dc8b8878ed3618fae275cf3229b6ee1b
Olga <obulat@gmail.com>
2020-07-09 12:10:23 +0300

    Use css for opening/closing menu
    - Menu is opened/closed using only css (invisible checkbox), works without js enabled
    - Accessibility added using javascript

--- python_docs_theme/static/menu.js
+++ python_docs_theme/static/menu.js
@@ -1,35 +1,54 @@
-
 document.addEventListener('DOMContentLoaded', function () {
-    const toggler = document.querySelector('.toggler');
+
+    // Make tables responsive by wrapping them in a div and making them scrollable
+    const tables = document.querySelectorAll('table.docutils');
+    tables.forEach(function(table){
+        table.outerHTML = '<div class="responsive-table__container">' + table.outerHTML + '</div>'
+    });
+
+    const togglerInput = document.querySelector('.toggler__input');
+    const togglerLabel = document.querySelector('.toggler__label');
     const sideMenu = document.querySelector('.menu-wrapper');
+    const menuItems = document.querySelectorAll('.menu')
     const doc = document.querySelector('.document');
+    const body = document.querySelector('body');
 
     function closeMenu() {
-        sideMenu.classList.remove('open');
-        toggler.classList.remove('active');
-        toggler.setAttribute("aria-expanded", 'false');
+        togglerInput.checked = false;
+        sideMenu.setAttribute("aria-expanded", 'false');
+        sideMenu.setAttribute('aria-hidden', 'true');
+        togglerLabel.setAttribute('aria-pressed', 'false');
+        body.style.overflow = 'visible';
     }
-    function isMenuOpen(){
-        return (' ' + sideMenu.className + ' ').indexOf(' open ') > -1;
+    function openMenu() {
+        togglerInput.checked = true;
+        sideMenu.setAttribute("aria-expanded", 'true');
+        sideMenu.setAttribute('aria-hidden', 'false');
+        togglerLabel.setAttribute('aria-pressed', 'true');
+        body.style.overflow = 'hidden';
     }
 
+    // Close menu when link on the sideMenu is clicked
     sideMenu.addEventListener('click', function (event) {
-        // Close menu only if a link on the sideMenu is clicked
         let target = event.target;
-        if (target.tagName !== 'a') return;
+        if (target.tagName.toLowerCase() !== 'a') return;
         closeMenu();
     })
-    toggler.addEventListener('click', function (e) {
-        if (!isMenuOpen()) {
-            sideMenu.classList.add('open');
-            toggler.classList.add('active');
-            toggler.setAttribute("aria-expanded", 'true');
-        } else {
-            closeMenu();
-        }
+    // Add accessibility data when sideMenu is opened/closed
+    togglerInput.addEventListener('change', function (e) {
+        togglerInput.checked ? openMenu() : closeMenu();
     });
+    // Make sideMenu links tabbable only when visible
+    for(let menuItem of menuItems) {
+        if(togglerInput.checked) {
+          menuItem.setAttribute('tabindex', '0');
+        } else {
+          menuItem.setAttribute('tabindex', '-1');
+        }
+    }
+    // Close sideMenu when document body is clicked
     doc.addEventListener('click', function () {
-        if (isMenuOpen()) {
+        if (togglerInput.checked) {
             closeMenu();
         }
     })
