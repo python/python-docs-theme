3bf3806b1f829b29ed5664b6772c1e9d163f270c
Septatrix <24257556+Septatrix@users.noreply.github.com>
2022-11-18 22:31:44 +0200

    Fix more bugs with the new mobile view

--- python_docs_theme/static/themetoggle.js
+++ python_docs_theme/static/themetoggle.js
@@ -1,39 +1,24 @@
-let currentTheme = localStorage.getItem('currentTheme') || 'auto'
-
-const pygmentsDark = document.getElementById('pygments_dark_css')
 const pydocthemeDark = document.getElementById('pydoctheme_dark_css')
+const pygmentsDark = document.getElementById('pygments_dark_css')
+const themeSelectors = document.getElementsByClassName('theme-selector')
 
-const themeOrder = {
-  light: 'dark',
-  dark: 'auto',
-  auto: 'light',
-}
-
-updateTheme()
-
-function toggleTheme() {
-  currentTheme = themeOrder[currentTheme]
-  localStorage.setItem('currentTheme', currentTheme)
-  updateTheme()
-}
-
-function updateTheme() {
-  const buttons = Array.from(document.getElementsByClassName('theme-toggle'))
-  switch (currentTheme) {
+function activateTheme(theme) {
+  localStorage.setItem('currentTheme', theme);
+  [...themeSelectors].forEach(e => e.value = theme)
+  switch (theme) {
     case 'light':
       pydocthemeDark.media = 'not all'
       pygmentsDark.media = 'not all'
-      buttons.forEach(e => e.value = "Toggle theme (light)")
       break;
     case 'dark':
       pydocthemeDark.media = 'all'
       pygmentsDark.media = 'all'
-      buttons.forEach(e => e.value = "Toggle theme (dark)")
       break;
     default:
       // auto
       pydocthemeDark.media = '(prefers-color-scheme: dark)'
       pygmentsDark.media = '(prefers-color-scheme: dark)'
-      buttons.forEach(e => e.value = "Toggle theme (auto)")
   }
 }
+
+activateTheme(localStorage.getItem('currentTheme') || 'auto')
