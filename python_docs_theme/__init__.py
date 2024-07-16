4a284c25bcf81c50dd7ab1fd9dc11b0bdde28c78
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2024-07-16 03:26:21 +0100

    Add language and version switchers

--- python_docs_theme/__init__.py
+++ python_docs_theme/__init__.py
@@ -2,14 +2,83 @@ from __future__ import annotations
 
 import hashlib
 import os
+import sys
 from functools import lru_cache
 from pathlib import Path
-from typing import Any
+from typing import Any, Literal
 
+import httpx
 import sphinx.application
 from sphinx.builders.html import StandaloneHTMLBuilder
 
+if sys.version_info[:2] >= (3, 11):
+    import tomllib
+else:
+    import tomli as tomllib
+
 THEME_PATH = Path(__file__).parent.resolve()
+
+
+def _version_label(
+    version_name: str,
+    status: Literal["feature", "prerelease", "bugfix", "security", "end-of-life"],
+) -> str:
+    if status == "feature":
+        return f"dev ({version_name})"
+    if status == "prerelease":
+        return f"pre ({version_name})"
+    if status in {"end-of-life", "security", "bugfix"}:
+        return version_name
+    msg = f"Unknown status: {status}"
+    raise ValueError(msg)
+
+
+def _builder_inited(app):
+    html_context = app.config.html_context
+    language = app.config.language
+    release = app.config.release
+    if app.config.html_theme != "python_docs_theme":
+        return
+
+    # Get the current branch statuses
+    releases = httpx.get(
+        "https://raw.githubusercontent.com/python/devguide/main/include/release-cycle.json",
+        timeout=30,
+    ).json()
+    # Get appropriate version labels
+    release_labels = {
+        name: _version_label(name, release["status"])
+        for name, release in releases.items()
+    }
+    # Update the current version to be the full release string
+    if (short_version := ".".join(release.split(".", 2)[:2])) in release_labels:
+        release_labels[short_version] = release
+
+    # Store the versions in the context as a sorted list of tuples
+    html_context["switchers_versions"] = sorted(
+        release_labels.items(),
+        key=lambda release_label: tuple(map(int, release_label[0].split("."))),
+        reverse=True,
+    )
+
+    # Get the languages from the docsbuild-scripts config
+    docsbuild_config = httpx.get(
+        "https://raw.githubusercontent.com/python/docsbuild-scripts/main/config.toml",
+        timeout=30,
+    ).text
+    # Convert language tags and extract language names
+    languages = [
+        (iso639_tag.replace("_", "-").lower(), section["name"])
+        for iso639_tag, section in tomllib.loads(docsbuild_config)["languages"].items()
+        if section.get("in_prod", True)
+    ]
+
+    # If we are working on a language that is not in the list, add it
+    if language and language not in dict(languages):
+        languages.append((language, language))
+
+    # Store the versions in the context as a sorted list of tuples
+    html_context["switchers_languages"] = sorted(languages)
 
 
 @lru_cache(maxsize=None)
@@ -56,6 +125,7 @@ def setup(app):
     current_dir = os.path.abspath(os.path.dirname(__file__))
     app.add_html_theme("python_docs_theme", current_dir)
 
+    app.connect("builder-inited", _builder_inited)
     app.connect("html-page-context", _html_page_context)
 
     return {
