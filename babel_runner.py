bd7c059d3f233b7e4c7035aba798135dcdf816e8
Rafael Fontenelle <rffontenelle@users.noreply.github.com>
2024-10-28 08:23:22 -0300

    Apply suggestions from code review
    Co-authored-by: Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
    Co-authored-by: Ezio Melotti <ezio.melotti@gmail.com>

--- babel_runner.py
+++ babel_runner.py
@@ -34,7 +34,7 @@ def get_project_info() -> dict:
     return data["project"]
 
 
-def extract_messages():
+def extract_messages() -> None:
     """Extract messages from all source files into message catalog template"""
     Path(PROJECT_DIR, LOCALES_DIR).mkdir(parents=True, exist_ok=True)
     project_data = get_project_info()
@@ -61,7 +61,7 @@ def extract_messages():
     )
 
 
-def init_locale(locale: str):
+def init_locale(locale: str) -> None:
     """Initialize a new locale based on existing message catalog template"""
     pofile = PROJECT_DIR / LOCALES_DIR / locale / "LC_MESSAGES" / f"{DOMAIN}.po"
     if pofile.exists():
@@ -71,23 +71,23 @@ def init_locale(locale: str):
     subprocess.run(cmd, cwd=PROJECT_DIR, check=True)
 
 
-def update_catalogs(locale: str):
+def update_catalogs(locale: str) -> None:
     """Update translations from existing message catalogs"""
     cmd = ["pybabel", "update", "-i", POT_FILE, "-d", LOCALES_DIR]
-    if locale != "":
+    if locale:
         cmd.extend(["-l", locale])
     subprocess.run(cmd, cwd=PROJECT_DIR, check=True)
 
 
-def compile_catalogs(locale: str):
+def compile_catalogs(locale: str) -> None:
     """Compile existing message catalogs"""
     cmd = ["pybabel", "compile", "-d", LOCALES_DIR]
-    if locale != "":
+    if locale:
         cmd.extend(["-l", locale])
     subprocess.run(cmd, cwd=PROJECT_DIR, check=True)
 
 
-def main():
+def main() -> None:
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
         "command",
@@ -97,16 +97,17 @@ def main():
     parser.add_argument(
         "-l",
         "--locale",
+        default="",
         help="language code (needed for init, optional for update and compile)",
     )
 
     args = parser.parse_args()
-    locale = args.locale if args.locale else ""
+    locale = args.locale
 
     if args.command == "extract":
         extract_messages()
     elif args.command == "init":
-        if locale == "":
+        if not locale:
             parser.error("init requires passing the --locale option")
         init_locale(locale)
     elif args.command == "update":
