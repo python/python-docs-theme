4f43aded34b3509c13b2f203cd5ff1496a7eeaab
Stan Ulbrych <89152624+StanFromIreland@users.noreply.github.com>
2025-09-07 18:10:55 +0100

    Enable theme translation (#246)
    Co-authored-by: Maciej Olko <maciej.olko@gmail.com>

--- python_docs_theme/__init__.py
+++ python_docs_theme/__init__.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from pathlib import Path
 
+from sphinx.locale import get_translation
+
 TYPE_CHECKING = False
 if TYPE_CHECKING:
     from sphinx.application import Sphinx
@@ -10,12 +12,21 @@ if TYPE_CHECKING:
 __version__ = "2025.5"
 
 THEME_PATH = Path(__file__).resolve().parent
+LOCALE_DIR = THEME_PATH / "locale"
+MESSAGE_CATALOG_NAME = "python-docs-theme"
 
 
 def setup(app: Sphinx) -> ExtensionMetadata:
     app.require_sphinx("7.3")
 
     app.add_html_theme("python_docs_theme", str(THEME_PATH))
+    app.add_message_catalog(MESSAGE_CATALOG_NAME, LOCALE_DIR)
+
+    def add_translation_to_context(app, pagename, templatename, context, doctree):
+        _ = get_translation(MESSAGE_CATALOG_NAME)
+        context["_"] = context["gettext"] = context["ngettext"] = _
+
+    app.connect("html-page-context", add_translation_to_context)
 
     return {
         "version": __version__,
