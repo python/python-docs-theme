44303e12a75c6d5e1319774a258f1d05b4c2518b
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2025-09-08 20:54:18 +0300

    Prepare 2025.9 release (#254)
    Co-authored-by: Stan Ulbrych <89152624+StanFromIreland@users.noreply.github.com>

--- python_docs_theme/__init__.py
+++ python_docs_theme/__init__.py
@@ -9,7 +9,7 @@ if TYPE_CHECKING:
     from sphinx.application import Sphinx
     from sphinx.util.typing import ExtensionMetadata
 
-__version__ = "2025.5"
+__version__ = "2025.9"
 
 THEME_PATH = Path(__file__).resolve().parent
 LOCALE_DIR = THEME_PATH / "locale"
