fee2ae0dbf6251e5df36f621a605085d9f6aa0ec
Stan Ulbrych <89152624+StanFromIreland@users.noreply.github.com>
2025-09-15 17:35:03 +0100

    Fix translation of html pages (#263)

--- python_docs_theme/__init__.py
+++ python_docs_theme/__init__.py
@@ -6,6 +6,8 @@ from sphinx.locale import get_translation
 
 TYPE_CHECKING = False
 if TYPE_CHECKING:
+    from typing import Any
+
     from sphinx.application import Sphinx
     from sphinx.util.typing import ExtensionMetadata
 
@@ -16,16 +18,30 @@ LOCALE_DIR = THEME_PATH / "locale"
 MESSAGE_CATALOG_NAME = "python-docs-theme"
 
 
+def add_translation_to_context(
+    app: Sphinx,
+    pagename: str,
+    templatename: str,
+    context: dict[str, Any],
+    doctree: None,
+) -> None:
+    theme_gettext = get_translation(MESSAGE_CATALOG_NAME)
+    sphinx_gettext = get_translation("sphinx")
+
+    def combined(message: str) -> str:
+        translation = theme_gettext(message)
+        if translation == message:
+            return sphinx_gettext(message)
+        return translation
+
+    context["_"] = context["gettext"] = context["ngettext"] = combined
+
+
 def setup(app: Sphinx) -> ExtensionMetadata:
     app.require_sphinx("7.3")
 
     app.add_html_theme("python_docs_theme", str(THEME_PATH))
     app.add_message_catalog(MESSAGE_CATALOG_NAME, LOCALE_DIR)
-
-    def add_translation_to_context(app, pagename, templatename, context, doctree):
-        _ = get_translation(MESSAGE_CATALOG_NAME)
-        context["_"] = context["gettext"] = context["ngettext"] = _
-
     app.connect("html-page-context", add_translation_to_context)
 
     return {
